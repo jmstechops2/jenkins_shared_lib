db43f44eaa8bbc2d2af9d6cfb44939bac3589796
jmstechops2 <jmstechops2@gmail.com>
2026-02-03 21:22:26 +0530

    Update common_seed.groovy

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,21 +1,36 @@
-def baseUrl = "https://github.com/jmstechops2/"
-def repoName = "$reponame"
-def gitRepoUrl= baseUrl + repoName + '.git'
-def jobName = "$reponame"
-  pipelineJob(jobName) {
+import groovy.json.JsonSlurper
+
+def jsonFile = new File("$jenkins/input_jobs.json")
+def config = new JsonSlurper().parse(jsonFile)
+
+def baseUrl = config.baseUrl
+
+config.repositories.each { repo ->
+
+    def repoName   = repo.name
+    def branchName = repo.branch
+    def scriptPath = repo.scriptPath
+
+    def gitRepoUrl = "${baseUrl}${repoName}.git"
+    def jobName    = repoName
+
+    pipelineJob(jobName) {
+
         properties {
-         pipelineTriggers {
-          triggers {
-            pollSCM {
-              scmpoll_spec('*/1 * * * *')
-              ignorePostCommitHooks(true)
+            pipelineTriggers {
+                triggers {
+                    pollSCM {
+                        scmpoll_spec('*/1 * * * *')
+                        ignorePostCommitHooks(true)
+                    }
+                }
             }
-          }
-         }
         }
-        logRotator{
-         numToKeep(5)
+
+        logRotator {
+            numToKeep(5)
         }
+
         definition {
             cpsScm {
                 scm {
@@ -24,13 +39,14 @@ def jobName = "$reponame"
                             url(gitRepoUrl)
                             credentials('github_credentials')
                         }
-                        branches('master')
+                        branches(branchName)
                         extensions {
                             cleanBeforeCheckout()
                         }
                     }
                 }
-                scriptPath("Jenkinsfile")
+                scriptPath(scriptPath)
             }
         }
     }
+}
